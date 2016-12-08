577d1855e53a8062a1cec470363a2291bb7cc59a
mehmettokak <tokakmehmet@gmail.com>
2016-12-08 17:59:33 +0200

    PdfViewer
    Mvc-PdfViewer-PdfJs

---
+++ Mvc-PdfViewer/Controllers/DocumentController.cs
@@ -0,0 +1,24 @@
+﻿using Mvc_PdfViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_PdfViewer.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Viewer(HttpPostedFileBase Doc)
+        {
+            if (Doc!=null)
+            {
+                var path = Get.GetFilePath("UploadFilePath") + "/Document/";
+                ViewBag.DocUrl = Get.SaveDocument(Doc, path);
+            }
+           
+            return View();
+        }
+    }
+}
---
+++ Mvc-PdfViewer/Models/Get.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_PdfViewer.Models
+{
+    public static class Get
+    {
+        public static string GetFilePath(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+        public static string SaveDocument(HttpPostedFileBase files, string folderPath)
+        {
+            try
+            {
+                if (!Directory.Exists(HttpContext.Current.Server.MapPath("~" + folderPath)))
+                    Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~" + folderPath));
+
+                var filePath = folderPath + files.FileName;
+                if (File.Exists(HttpContext.Current.Server.MapPath("~" + filePath)))
+                    File.Delete(HttpContext.Current.Server.MapPath("~" + filePath));
+
+                files.SaveAs(HttpContext.Current.Server.MapPath("~" + filePath));
+               
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
